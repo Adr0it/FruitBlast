f20401af355be549f8ece4d8a71443138efa58b9
Adr0it <46908309+Adr0it@users.noreply.github.com>
2025-12-31 08:36:34 -0600

    fruitblast

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 // define variables
-const display = document.querySelector("#display");
-const spin = document.querySelector("#spin");
-const profit = document.querySelector("#profit");
+const display = document.querySelector("#display")
+const spin = document.querySelector("#spin")
+const profit = document.querySelector("#profit")
 
 const fruits = ["apple", "banana", "grape", "orange", "pineapple", "blueberry", "cherry"]
 const costs = {
@@ -16,26 +16,26 @@ const costs = {
 
 // add cells to display
 for (let i = 0; i < 64; i++) {
-    const cell = document.createElement("div");
-    cell.className = "cell";
-    cell.id = (i+1);
-    display.appendChild(cell);
+    const cell = document.createElement("div")
+    cell.className = "cell"
+    cell.id = (i+1)
+    display.appendChild(cell)
 }
-var cells = document.querySelectorAll(".cell");
+var cells = document.querySelectorAll(".cell")
 
 // delay helper method
 function delay(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise(resolve => setTimeout(resolve, ms))
 }
 
 // clear cells
 function clear() {
-    profit.textContent = "Profit: 0";
+    profit.textContent = "Profit: 0"
     fruits.forEach((f) => {
-        let t = document.querySelectorAll("." + f);
+        let t = document.querySelectorAll("." + f)
         for (let i = 0; i < t.length; i++) {
-            t[i].remove();
-            let cell = document.getElementById(t[i].id.toString());
+            t[i].remove()
+            let cell = document.getElementById(t[i].id.toString())
             cell.style.backgroundColor = "#DBFFD6"
         }
     })
@@ -44,37 +44,41 @@ function clear() {
 // fill cells
 async function fill_empty() {
     for (let i = 64; i > 0; i--) {
-        let f = document.createElement("div");
+        let f = document.createElement("div")
 
-        let sel = Math.floor(Math.random()*fruits.length);
-        f.classList.add(fruits[sel]);
-        f.id = i;
-        f.style.marginLeft = 61.6*((i+7) % 8) + "px";
-        f.style.transition = "transform 2s ease-in-out";
-        f.style.transform = "translateY(-30px)";
-        display.appendChild(f);
+        let sel = Math.floor(Math.random()*fruits.length)
+        f.classList.add(fruits[sel])
+        f.id = i
+        f.style.marginLeft = 61.6*((i+7) % 8) + "px"
+        f.style.transition = "transform 2s ease-in-out"
+        f.style.transform = "translateY(-30px)"
+        display.appendChild(f)
 
-        await delay(20);
+        await delay(20)
 
         requestAnimationFrame(() => {
-            f.style.transform = "translateY(" + 61.6*(Math.ceil(i / 8)-1) + "px)";
-        });
+            f.style.transform = "translateY(" + 61.6*(Math.ceil(i / 8)-1) + "px)"
+        })
     }
 }
 
 // matching helper method
-function seek_squares(ar, sq, out) {
-    if (!ar.includes(sq)) return out; // not in target array
-    if (out.includes(sq)) return out; // already visited
+function seek_squares(ar, sq, out = []) {
+    if (!ar.includes(sq)) return out
+    if (out.includes(sq)) return out
 
-    out.push(sq);
+    out.push(sq)
 
-    seek_squares(ar, sq - 1, out);
-    seek_squares(ar, sq - 8, out);
-    seek_squares(ar, sq + 1, out);
-    seek_squares(ar, sq + 8, out);
+    seek_squares(ar, sq - 1, out)
+    seek_squares(ar, sq + 1, out)
+    seek_squares(ar, sq - 8, out)
+    seek_squares(ar, sq + 8, out)
 
-    // additional checks for proper matching detection
+    return out
+}
+
+// additional checks for proper matching detection
+function detect_match(out) {
     h = []
     v = []
     for (let i = 0; i < out.length; i++) {
@@ -106,42 +110,44 @@ function seek_squares(ar, sq, out) {
     return h.concat(v);
 }
 
+
 // matching algorithm
 function check_matches() {
     let p = 0
     fruits.forEach((f) => {
         let visited_squares = []
-        let t = document.querySelectorAll("." + f);
-        input = [];
+        let t = document.querySelectorAll("." + f)
+        input = []
         for (let i = 0; i < t.length; i++) {
-            input.push(parseInt(t[i].id));
+            input.push(parseInt(t[i].id))
         }
-        console.log(f + ": " + input);
+        console.log(f + ": " + input)
 
         for (let i = 0; i < input.length; i++) {
-            console.log(input[i]);
-            const res = seek_squares(input, input[i], []);
-            if (res == null) continue
+            const connected = seek_squares(input, input[i])
+            const match = detect_match(connected)
+            console.log(input[i], match)
 
-            for (let j = 0; j < res.length; j++) {
-                if (visited_squares.includes(res[j]))
-                    continue;
-                visited_squares.push(res[j])
+            if (match == null) continue
+
+            for (let j = 0; j < match.length; j++) {
+                if (visited_squares.includes(match[j]))
+                    continue
+                visited_squares.push(match[j])
                 p += costs[f]
-                let cell = document.getElementById(res[j].toString());
-                cell.style.backgroundColor = "#59a35dff";
+                let cell = document.getElementById(match[j].toString())
+                cell.style.backgroundColor = "#46ff50ff"
             }
-            console.log(res);
         }
     })
-    profit.textContent = "Profit: " + p;
+    profit.textContent = "Profit: " + p
 }
 
-// spin mechanism
+// spin mechanism 
 async function spin_clicked() {
-    clear();
-    fill_empty();
-    await delay (4100);
-    check_matches();
+    clear()
+    fill_empty()
+    await delay (4100)
+    check_matches()
 }
-spin.addEventListener("click", spin_clicked);
+spin.addEventListener("click", spin_clicked)
